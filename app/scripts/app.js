88cd7c643cddbbeb5ff42b3a9f10e2bc9caeaf0d
John Dill <dill.john.d@gmail.com>
2014-09-11 16:22:42 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name vizwizApp
+ * @description
+ * # vizwizApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('vizwizApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
