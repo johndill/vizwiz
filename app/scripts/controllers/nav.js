d0827342499da96421e28ab4b8439558c140d358
John Dill <dill.john.d@gmail.com>
2014-09-14 16:55:02 -0400

    worked on nav controller

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('vizwizApp')
-  .controller('NavCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+  .controller('NavCtrl', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
   	$scope.logout = function () {
   		$rootScope.loggedIn = false;
   	};
@@ -9,4 +9,6 @@ angular.module('vizwizApp')
   	$scope.login = function () {
   		$rootScope.loggedIn = true;
   	};
+  	$scope.showSignup = $location.path() === '/login';
+  	$scope.showLogin = $location.path() === '/signup';
   }]);
