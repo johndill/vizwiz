8e65ae2f10af167aa952520ab18f0c3aa1c55933
John Dill <dill.john.d@gmail.com>
2014-09-21 15:47:19 -0400

    fit handsontable to data area

--- app/scripts/directives/handsontable.js
+++ app/scripts/directives/handsontable.js
@@ -8,12 +8,27 @@ angular.module('vizwizApp')
       restrict: 'A',
       link: function(scope, element) {
         var data = scope.data;
+        var tableHeight = $(window).height() - 215 - 135 * scope.myData.open;
+        
+        element.height(tableHeight);
         $(element).handsontable({
           data: data,
           colHeaders: true,
           rowHeaders: true,
           minSpareRows: 1,
           contextMenu: true
+        });
+        
+        scope.$watch('myData.open', function(newVal) {
+          tableHeight = $(window).height() - 215 - 135 * newVal;
+          element.height(tableHeight);
+          $(element).handsontable('render');
+        });
+        
+        $(window).on('resize', function() {
+          tableHeight = $(window).height() - 215 - 135 * scope.myData.open;
+          element.height(tableHeight);
+          $(element).handsontable('render');
         });
       }
     };
