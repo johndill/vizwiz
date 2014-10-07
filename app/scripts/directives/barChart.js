e12985c0affad60fa6258a15248ad8cd800caa67
John Dill <dill.john.d@gmail.com>
2014-10-06 21:59:17 -0400

    made visualization interactive

--- app/scripts/directives/barChart.js
+++ app/scripts/directives/barChart.js
@@ -6,9 +6,35 @@ angular.module('vizwizApp')
       restrict: 'EA',
       link: function(scope, element, attrs) {
         d3Service.d3().then(function(d3) {
-          var margin = parseInt(attrs.margin) || 20,
+          var margin = parseInt(attrs.margin) || 0,
               barHeight = parseInt(attrs.barHeight) || 20,
               barPadding = parseInt(attrs.barPadding) || 5;
+          
+          d3.select(element[0])
+            .append('span')
+            .text('Show ');
+          
+          var seriesDropdown = d3.select(element[0])
+            .append('select')
+            .attr('name', 'series-list');
+          seriesDropdown.on('change', function() {
+            scope.render(scope.vizData);
+          });
+          
+          d3.select(element[0])
+            .append('span')
+            .text(' by ');
+          
+          var categoryDropdown = d3.select(element[0])
+            .append('select')
+            .attr('name', 'category-list');
+          categoryDropdown.on('change', function() {
+            scope.render(scope.vizData);
+          });
+          
+          d3.select(element[0])
+            .append('div')
+            .style({'height': '10px'});
           
           var svg = d3.select(element[0])
             .append('svg')
@@ -34,6 +60,7 @@ angular.module('vizwizApp')
           });
           
           scope.render = function (data) {
+            
             // remove all previous items before render
             svg.selectAll('*').remove();
             
@@ -52,9 +79,25 @@ angular.module('vizwizApp')
                   // xScale
                   xScale = d3.scale.linear()
                     .domain([0, d3.max(data, function(d) {
-                      return d.Mazda;
+                      return d[seriesDropdown[0][0].value];
                     })])
                     .range([0, width]);
+              
+              // update series dropdown
+              seriesDropdown.selectAll('option')
+                .data(Object.getOwnPropertyNames(data[0]))
+                .enter()
+                .append('option')
+                .text(function (d) { return d; })
+                .attr('value', function(d) { return d; });
+              
+              // update category dropdown
+              categoryDropdown.selectAll('option')
+                .data(Object.getOwnPropertyNames(data[0]))
+                .enter()
+                .append('option')
+                .text(function (d) { return d; })
+                .attr('value', function(d) { return d; });
 
               // set the height based on calcs above
               svg.attr('height', height);
@@ -69,23 +112,30 @@ angular.module('vizwizApp')
                   .attr('y', function (d, i) {
                     return i * (barHeight + barPadding);
                   })
-                  .attr('fill', function(d) { return color(d.Mazda); })
+                  .attr('fill', function(d) {
+                    return color(d[seriesDropdown[0][0].value]); 
+                  })
                   .transition()
                     .duration(1000)
                     .attr('width', function(d) {
-                      return xScale(d.Mazda);
-                  });
+                      return xScale(d[seriesDropdown[0][0].value]);
+                    });
               svg.selectAll('text')
                 .data(data)
                 .enter()
                   .append('text')
-                  .attr('fill', '#fff')
+                  .attr('fill', '#000')
                   .attr('y', function(d, i) {
                     return i * (barHeight + barPadding) + 15;
-                })
-                .attr('x', 15)
+                  })
+                  .attr('x', 145)
+                  .transition()
+                    .duration(1000)
+                    .attr('x', function(d) {
+                      return xScale(d[seriesDropdown[0][0].value]) + 5;
+                    })
                 .text(function(d) {
-                  return d.Year + ' (Mazda Sold: ' + d.Mazda + ')';
+                  return d[categoryDropdown[0][0].value] + ': ' + d[seriesDropdown[0][0].value];
                 });
             }, 200);
           };
